b7cc975da19edf18aa696fe50a901c28b224ee02
Administrator <Administrator@PC233>
2020-06-18 16:10:05 +0700

    update following

--- YLongNieKDam_lab4/Controllers/CoursesController.cs
+++ YLongNieKDam_lab4/Controllers/CoursesController.cs
@@ -68,6 +68,21 @@ namespace YLongNieKDam_lab4.Controllers
             };
             return View(viewModel);
         }
+        public ActionResult Following()
+        {
+            var userId = User.Identity.GetUserId();
+            var followings = _dbContext.Followings
+                .Where(a => a.FolloweeId == userId)
+                .Select(a => a.Follower)
+                .ToList();
+            var viewModel = new FollowingViewModel
+            {
+                Followings = followings,
+                ShowAction = User.Identity.IsAuthenticated
+            };
+            return View(viewModel);
+        }
+    
         [Authorize]
         public ActionResult Mine()
         {
